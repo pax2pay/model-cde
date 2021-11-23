96dae2502cfe5324e7d3c557b4fca1fb16fd56d9
Christoffer Johansson <christoffer@issuefab.com>
2021-11-23 14:01:24 +0100

    Selector with working parse.

--- Proxy/Selector.ts
+++ Proxy/Selector.ts
@@ -1,1 +1,31 @@
 export type Selector = string
+
+export namespace Selector {
+	function parse(selector: Selector): (string | number)[] {
+		return selector
+			.split(".")
+			.flatMap(property => (property.endsWith("]") ? property.split("[") : property))
+			.map(property => (property.endsWith("]") ? Number.parseInt(property.substr(0, property.length - 1)) : property))
+	}
+	export function get(data: any, selector: Selector | (string | number)[]): any {
+		let result: any
+		if (typeof selector == "string")
+			result = get(data, parse(selector))
+		else
+			result = selector.length == 0 ? data : get(data[selector[0]], selector.slice(1))
+		return result
+	}
+	export function set(data: any, selector: Selector | (string | number)[], value: any): any {
+		let result: any
+		if (typeof selector == "string")
+			result = set(data, parse(selector), value)
+		else
+			result =
+				selector.length == 0
+					? value
+					: Object.assign(Array.isArray(data) ? [...data] : { ...data }, {
+							[selector[0]]: set(data[selector[0]], selector.slice(1), value),
+					  })
+		return result
+	}
+}
