596ad0b8f190ab02c4bf21354921baa9f81e664b
ChriJ <christofferj93@gmail.com>
2021-11-24 11:07:53 +0100

    More proxy (#2)
    * Refactored Configuration, added to Selector.
    
    * Refactored and changed names, exports.
    
    * Newer node version, required for replaceAll.
    
    Co-authored-by: Christoffer Johansson <christoffer@issuefab.com>

--- Card/Expires/index.ts
+++ Card/Expires/index.ts
@@ -8,6 +8,13 @@ export namespace Expires {
 	export function is(value: Expires | any): value is Expires {
 		return Array.isArray(value) && value.length == 2 && ExpiresMonth.is(value[0]) && ExpiresYear.is(value[1])
 	}
+	export function stringify(expires: Expires): string {
+		return expires[0] + "/" + expires[1]
+	}
+	export function parse(expires: string): Expires | undefined {
+		const result = expires.split("/").map(Number.parseInt)
+		return Expires.is(result) ? result : undefined
+	}
 	export type Month = ExpiresMonth
 	export namespace Month {
 		export const is = ExpiresMonth.is
---
+++ Proxy/Configuration/index.ts
@@ -0,0 +1,20 @@
+import { Request as ConfigurationRequest } from "./Request"
+import { Response as ConfigurationResponse } from "./Response"
+
+export interface Configuration {
+	id: string
+	request: ConfigurationRequest
+	response: ConfigurationResponse.Json
+}
+
+export namespace Configuration {
+	export type Request = ConfigurationRequest
+	export type Response = ConfigurationResponse
+	export namespace Response {
+		export type Json = ConfigurationResponse.Json
+		export namespace Json {
+			export const extract = ConfigurationResponse.Json.extract
+			export const process = ConfigurationResponse.Json.process
+		}
+	}
+}
