e31eb5c6d04ec88997223896134f370ab700c997
Christoffer Johansson <christoffer@issuefab.com>
2021-12-14 13:14:35 +0100

    Added to Pattern, still wip.

--- Proxy/Pattern.ts
+++ Proxy/Pattern.ts
@@ -1,1 +1,57 @@
 export type Pattern = RegExp | string
+
+export namespace Pattern {
+	export function is(value: Pattern | any): value is Pattern {
+		return typeof value == "string" || value instanceof RegExp
+	}
+	export function get(data: string, pattern: Pattern): string | undefined {
+		let result: string | null
+		if (typeof pattern == "string") {
+			const matched = data.match(new RegExp(pattern + ">" + "[^<]*"))
+			if (matched == null) {
+				throw Error
+			} else {
+				result = matched[0].toString()
+			}
+		} else {
+			const matched = data.match(new RegExp(pattern.source.toString() + ">" + "[^<]*"))
+			if (matched == null) {
+				throw Error
+			} else {
+				result = matched[0].toString()
+			}
+		}
+
+		return result != null ? result.substr(result.indexOf(">") + 1) : ""
+	}
+
+	export function set(data: string, pattern: Pattern, value: string): string {
+		let result: string | null
+		let matched
+		if (typeof pattern == "string") {
+			matched = data.matchAll(new RegExp("<" + pattern + ">", "g"))
+			if (matched == null) {
+				throw Error
+			}
+		} else {
+			matched = data.matchAll(new RegExp("<" + pattern.source.toString() + ">", "g"))
+			if (matched == null) {
+				throw Error
+			}
+		}
+
+		for (const match of matched) {
+			console.log(match)
+			if (match.index) {
+				const length = match[0].length
+				const firstIndex = match.index + match[0].length
+				const nextIndex = data.substring(match.index + match[0].length).indexOf("<")
+
+				data = data.replace(data.slice(firstIndex, firstIndex + nextIndex), value)
+			}
+		}
+
+		result = data
+		return result
+	}
+}
