c1a6a645087e1b339b2f9d403512199df183ed15
Christoffer Johansson <christoffer@issuefab.com>
2022-01-19 14:07:55 +0100

    Added type to configs.

--- Proxy/Configuration/Tokenize/index.ts
+++ Proxy/Configuration/Tokenize/index.ts
@@ -1,8 +1,11 @@
 import { Json as TokenizeJson } from "./Json"
+import { Text as TokenizeText } from "./Text"
 
 export type Tokenize = TokenizeJson
 
 export namespace Tokenize {
 	export type Json = TokenizeJson
 	export const Json = TokenizeJson
+	export type Text = TokenizeText
+	export const Text = TokenizeText
 }
