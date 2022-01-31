d5b364ce50171d9a0a98cdfec6bd31449e2c8190
Christoffer Johansson <christoffer@issuefab.com>
2022-01-31 10:39:43 +0100

    Separating reverse configs and normal configs.

--- Proxy/Configuration/index.ts
+++ Proxy/Configuration/index.ts
@@ -1,10 +1,17 @@
 import { Request as ConfigurationRequest } from "./Request"
 import { Response as ConfigurationResponse } from "./Response"
 
-export interface Configuration {
+export type Configuration = NormalConfiguration | ReverseConfiguration
+
+interface NormalConfiguration {
 	id: string
 	request: ConfigurationRequest
 	response: ConfigurationResponse.Json
+}
+
+interface ReverseConfiguration {
+	id: string
+	request: ConfigurationRequest
 }
 
 export namespace Configuration {
