00c5eef1fc9d57befae77947b1fe1492c691e1af
ChriJ <christofferj93@gmail.com>
2023-01-09 14:24:26 +0100

    fix test after refactor

--- Proxy/Configuration/Tokenize/Text/test/index.spec.ts
+++ Proxy/Configuration/Tokenize/Text/test/index.spec.ts
@@ -2,13 +2,13 @@ import * as model from "../../../../../index"
 import { configurations } from "./configurations"
 import { dataset } from "./dataset"
 
-describe("@pax2pay/model.Proxy.Configuration.Response.Text", () => {
+describe("@pax2pay/model.Proxy.Configuration.Tokenize.Text", () => {
 	it("extract", async () => {
 		const data = dataset["generic"]
 		const config = configurations["expires"]
-		expect(model.Proxy.Configuration.Response.Text.is(config)).toBeTruthy()
+		expect(model.Proxy.Configuration.Tokenize.Text.is(config)).toBeTruthy()
 
-		const card = model.Proxy.Configuration.Response.Text.extract(config, data)
+		const card = model.Proxy.Configuration.Tokenize.Text.extract(config, data)
 		expect(card).toMatchObject({
 			pan: "1234123412341234",
 			csc: "987",
@@ -19,11 +19,11 @@ describe("@pax2pay/model.Proxy.Configuration.Response.Text", () => {
 	it("process regular", async () => {
 		const data = dataset["generic"]
 		const config = configurations["expires"]
-		expect(model.Proxy.Configuration.Response.Text.is(config)).toBeTruthy()
+		expect(model.Proxy.Configuration.Tokenize.Text.is(config)).toBeTruthy()
 
 		const token = "1234121234/16/1224/1354/0ktG52FXmULx7-3mrj0smEWvJWwuJNA9eQNr8O8kBBKy_gvg/FlBUNQjpk4R9g_dcw6WYzQ"
 
-		const result = model.Proxy.Configuration.Response.Text.process(config, token, data)
+		const result = model.Proxy.Configuration.Tokenize.Text.process(config, token, data)
 		const expected =
 			"<card>\n\t<cardNumber>123412******1234</cardNumber>\n\t<csc></csc>\n\t<someOtherInfo>random information</someOtherInfo>\n\t<expires>12/24</expires>\n</card>"
 		expect(result).toEqual(expected)
@@ -32,11 +32,11 @@ describe("@pax2pay/model.Proxy.Configuration.Response.Text", () => {
 	it("process double field", async () => {
 		const data = dataset["doubleNumber"]
 		const config = configurations["expires"]
-		expect(model.Proxy.Configuration.Response.Text.is(config)).toBeTruthy()
+		expect(model.Proxy.Configuration.Tokenize.Text.is(config)).toBeTruthy()
 
 		const token = "1234121234/16/1224/1354/0ktG52FXmULx7-3mrj0smEWvJWwuJNA9eQNr8O8kBBKy_gvg/FlBUNQjpk4R9g_dcw6WYzQ"
 
-		const result = model.Proxy.Configuration.Response.Text.process(config, token, data)
+		const result = model.Proxy.Configuration.Tokenize.Text.process(config, token, data)
 		const expected =
 			"<card>\n\t<cardNumber>123412******1234</cardNumber>\n\t<csc></csc>\n\t<someOtherInfo>random information</someOtherInfo>\n\t<cardNumber>123412******1234</cardNumber>\n\t<expires>12/24</expires>\n</card>"
 		expect(result).toEqual(expected)
