6b92b6247e4f6f17c7de11a6719cecb8dd890ded
ChriJ <christofferj93@gmail.com>
2023-08-04 12:16:00 +0200

    worker-cde#117 - Prepare for banking integration (#32)
    * worker-cde#117 - Make marqeta integration possible
    
    * support iso dates in expires

--- Proxy/Configuration/index.ts
+++ Proxy/Configuration/index.ts
@@ -3,6 +3,7 @@ import { Tokenize as ConfigurationTokenize } from "./Tokenize"
 export interface Configuration {
 	authentication?: string | (string | undefined)[]
 	proxyAuthentication?: string
+	additionalHeaders?: Record<string, string>
 	url: string
 	request?: ConfigurationTokenize.Json | ConfigurationTokenize.Text | "detokenize"
 	response?: ConfigurationTokenize.Json | ConfigurationTokenize.Text | "detokenize"
