9e6c15dee88259c7ee13c9c23ee7af30b147e02e
Qijie Yu <qijie@issuefab.com>
2024-01-26 10:50:21 +0100

    accept row data to generate image and pdf and remove cloudly-rest

--- Client/Card.ts
+++ Client/Card.ts
@@ -1,27 +1,37 @@
 import * as gracely from "gracely"
 import * as http from "cloudly-http"
-import * as rest from "cloudly-rest"
 import { Card as modelCard } from "../Card"
 
-export class Card extends rest.Collection<gracely.Error> {
+export class Card extends http.Client<gracely.Error> {
 	async detokenize(token: modelCard.Token): Promise<modelCard | string | gracely.Error> {
-		return await this.client.get<modelCard | string>(`card/${token}`)
+		return await this.get<modelCard | string>(`card/${token}`)
 	}
 	async getImage(token: modelCard.Token, cardHolderName?: string): Promise<string | gracely.Error> {
 		const queryString = cardHolderName ? `?ch=${cardHolderName}` : ""
-		return await this.client.get<string>(`card/${token}${queryString}`, { accept: ["image/svg+xml"] })
+		return await this.get<string>(`card/${token}${queryString}`, { accept: ["image/svg+xml"] })
 	}
 	async getPdf(token: modelCard.Token, cardHolderName?: string): Promise<string | gracely.Error> {
 		const queryString = cardHolderName ? `?ch=${cardHolderName}` : ""
-		return await this.client.get<string>(`card/${token}${queryString}`, { accept: ["application/pdf"] })
+		return await this.get<string>(`card/${token}${queryString}`, { accept: ["application/pdf"] })
+	}
+	async getImageWithRawData(card: modelCard, cardHolderName?: string): Promise<string | gracely.Error> {
+		const queryString = cardHolderName ? `?ch=${cardHolderName}` : ""
+		return await this.post<string>(`card/raw${queryString}`, card, { accept: ["image/svg+xml"] })
+	}
+	async getPdfWithRawData(card: modelCard, cardHolderName?: string): Promise<ArrayBuffer | gracely.Error> {
+		const queryString = cardHolderName ? `?ch=${cardHolderName}` : ""
+		const response = await this.post<ArrayBuffer>(`card/raw${queryString}`, card, {
+			accept: ["application/pdf"],
+		})
+		return response
 	}
 	async tokenize(card: modelCard): Promise<modelCard.Token | gracely.Error> {
-		return await this.client.post<modelCard.Token>("card", card)
+		return await this.post<modelCard.Token>("card", card)
 	}
 	async modify(token: modelCard.Token, card?: Partial<modelCard>): Promise<string | gracely.Error> {
-		return await this.client.patch<string>(`card/${token}`, card ?? {})
+		return await this.patch<string>(`card/${token}`, card ?? {})
 	}
-	static create(connection: http.Client): { card: Card } {
+	static create(connection: string): { card: Card } {
 		return { card: new Card(connection) }
 	}
 }
