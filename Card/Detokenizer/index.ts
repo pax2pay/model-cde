8eab1ab9bd69ef685a1fb2ae730702f4d1952376
ChriJ <christofferj93@gmail.com>
2024-01-31 15:12:23 +0100

    model-cde#43 - move tokenizing/detokenizing code from worker-cde to model-cde - small adjustments for worker compatibility (#47)
    * model-cde#43 - move tokenizing/detokenizing code from worker-cde to model-cde - small adjustments for worker compatibility
    
    * review, remove unused arguments

--- Card/Detokenizer/index.ts
+++ Card/Detokenizer/index.ts
@@ -2,6 +2,8 @@ import { Base as DetokenizerBase } from "./Base"
 import { Rsa as DetokenizerRsa } from "./Rsa"
 
 export namespace Detokenizer {
+	export type Base = DetokenizerBase
 	export const Base = DetokenizerBase
+	export type Rsa = DetokenizerRsa
 	export const Rsa = DetokenizerRsa
 }
