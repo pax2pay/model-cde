3e65876c885aaf722d0a830783f84dc267f8b062
Simon Mika <simon@mika.se>
2024-05-21 16:43:43 +0200

    Added tolerance for key length.

--- Key/index.spec.ts
+++ Key/index.spec.ts
@@ -3,8 +3,8 @@ import { pax2pay } from "../index"
 describe("pax2pay.cde.Key", () => {
 	it("generate", async () => {
 		expect(await pax2pay.cde.Key.generate()).toMatchObject({
-			private: expect.stringMatching(/^MIIE[a-zA-z0-9+/=]{1620}$/),
-			public: expect.stringMatching(/^MIIB[a-zA-z0-9+/=]{388}$/),
+			private: expect.stringMatching(/^MIIE[a-zA-z0-9+/=]{1600,1680}$/),
+			public: expect.stringMatching(/^MIIB[a-zA-z0-9+/=]{370,390}$/),
 		})
 	})
 })
