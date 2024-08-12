a296799b26007a5465b91b27dd3dabdcbfe70b4f
Christoffer Johansson <christoffer.johansson@pax2pay.com>
2024-08-12 09:50:58 +0200

    fix test descriptions

--- Card/Expires/index.spec.ts
+++ Card/Expires/index.spec.ts
@@ -11,7 +11,6 @@ describe("Card.Expires", () => {
 
 	it("parse 12/24", () => expect(pax2pay.cde.Card.Expires.parse("12/24")).toEqual([12, 24]))
 	it("parse 1224", () => expect(pax2pay.cde.Card.Expires.parse("1224")).toEqual([12, 24]))
-	it("parse 1224", () => expect(pax2pay.cde.Card.Expires.parse("1224")).toEqual([12, 24]))
 
 	it("parse 122", () => expect(pax2pay.cde.Card.Expires.parse("122")).toEqual([1, 22]))
 	it("parse 5/2023", () => expect(pax2pay.cde.Card.Expires.parse("5/2023")).toEqual([5, 23]))
@@ -21,15 +20,14 @@ describe("Card.Expires", () => {
 	it("parse 2033-07-01T12:34:56", () => expect(pax2pay.cde.Card.Expires.parse("2033-07-01T12:34:56")).toEqual([7, 33]))
 
 	//year first value
-	it("parse 12-24", () => expect(pax2pay.cde.Card.Expires.parse("24-12", false)).toEqual([12, 24]))
+	it("parse 24-12", () => expect(pax2pay.cde.Card.Expires.parse("24-12", false)).toEqual([12, 24]))
 
-	it("parse 12.24", () => expect(pax2pay.cde.Card.Expires.parse("24.12", false)).toEqual([12, 24]))
+	it("parse 24.12", () => expect(pax2pay.cde.Card.Expires.parse("24.12", false)).toEqual([12, 24]))
 
-	it("parse 12/24", () => expect(pax2pay.cde.Card.Expires.parse("24/12", false)).toEqual([12, 24]))
-	it("parse 1224", () => expect(pax2pay.cde.Card.Expires.parse("2412", false)).toEqual([12, 24]))
-	it("parse 1224", () => expect(pax2pay.cde.Card.Expires.parse("2412", false)).toEqual([12, 24]))
+	it("parse 24/12", () => expect(pax2pay.cde.Card.Expires.parse("24/12", false)).toEqual([12, 24]))
+	it("parse 2412", () => expect(pax2pay.cde.Card.Expires.parse("2412", false)).toEqual([12, 24]))
 
-	it("parse 122", () => expect(pax2pay.cde.Card.Expires.parse("221", false)).toEqual([1, 22]))
-	it("parse 5/2023", () => expect(pax2pay.cde.Card.Expires.parse("2023/5", false)).toEqual([5, 23]))
-	it("parse 52023", () => expect(pax2pay.cde.Card.Expires.parse("20235", false)).toEqual([5, 23]))
+	it("parse 221", () => expect(pax2pay.cde.Card.Expires.parse("221", false)).toEqual([1, 22]))
+	it("parse 2023/5", () => expect(pax2pay.cde.Card.Expires.parse("2023/5", false)).toEqual([5, 23]))
+	it("parse 20235", () => expect(pax2pay.cde.Card.Expires.parse("20235", false)).toEqual([5, 23]))
 })
