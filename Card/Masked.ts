df954aa879fba1773e7628cd543e798e2f220153
Christoffer Johansson <christoffer.johansson@pax2pay.com>
2025-04-28 16:46:41 +0200

    model-cde#61 - Add new 8 digit bins to Masked.ts

--- Card/Masked.ts
+++ Card/Masked.ts
@@ -1,5 +1,6 @@
 import * as isoly from "isoly"
 import { isly } from "isly"
+import { eightDigitBins } from "../eightDigitBins"
 import { Expires } from "./Expires"
 import { Card } from "./index"
 
@@ -25,7 +26,7 @@ export namespace Masked {
 	export const flaw = type.flaw
 	export function mask(card: Card): Masked {
 		const length = card.pan.length
-		const iinLength = ["45561800", "45561900", "49299300", "49299400"].some(iin => card.pan.startsWith(iin)) ? 8 : 6
+		const iinLength = eightDigitBins.some(iin => card.pan.startsWith(iin)) ? 8 : 6
 		const iin = card.pan.slice(0, iinLength)
 		const last4 = card.pan.slice(length - 4, length)
 		const result: Masked = {
